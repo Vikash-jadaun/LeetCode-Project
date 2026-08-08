06237d573d34954e39b16e5a901e2f133978cc30
Vikash Jadaun <jadaunvikash9@gmail.com>
2026-08-08 18:52:14 +0530

    Update Backtend

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -13,7 +13,7 @@ const cors=require('cors')
 
 
 app.use(cors({
-  origin:'http://localhost:5173',
+  origin:'https://leetcode-project-yzas.onrender.com',
   credentials:true
 }))
 app.use(express.json());
