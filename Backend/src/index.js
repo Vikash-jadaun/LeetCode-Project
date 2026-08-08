fc37b0672abc7c774001f6a3f5319a590138ad82
Vikash Jadaun <jadaunvikash9@gmail.com>
2026-08-08 19:50:48 +0530

    Update CORS for new Vercel domain

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -15,7 +15,7 @@ const cors=require('cors')
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://leetcode-project-pied.vercel.app'
+    'https://vikash-jadaun--leetcode-project-pied.vercel.app'
   ],
   credentials: true
 }))
